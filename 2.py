8e2b848ad2eef0b9a2c222ab8ae9eec74e6bdd0b
schreera <81811318+schreera@users.noreply.github.com>
2023-08-20 12:21:38 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,7 +4,8 @@ from multiprocessing.pool import ThreadPool
 
 def log_to_file(message, filename='log.txt'):
     with open(filename, 'a', encoding='utf-8') as f:
-        f.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {message}\n")
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+        f.write(f"{current_time} - {message}\n")
         
 def is_hex(s):
     try:
@@ -12,7 +13,6 @@ def is_hex(s):
         return True
     except ValueError:
         return False
-
 
 def read_private_keys():
     private_keys = []
@@ -24,11 +24,12 @@ def read_private_keys():
                     private_keys.append(key.strip())
                 else:
                     log_to_file(f"Invalid key found: {key}")
-                    print(f"Invalid key found: {key}")
+        num_keys = len(private_keys)
+        log_to_file(f"Loaded {num_keys} private keys. Start")
+        print(f"Loaded {num_keys} private keys. Start")
         return private_keys
     except Exception as err:
         log_to_file(f"Failed to read private_keys.txt - {err}")
-        print(f"Failed to read private_keys.txt - {err}")
         return []
 
 def auto_withdraw(to_address):
@@ -45,7 +46,6 @@ def auto_withdraw(to_address):
         to_address = web3_instance.toChecksumAddress(to_address.lower())
     except Exception as err:
         log_to_file(f"Error in address to withdraw - {err}")
-        print(f"Error in address to withdraw - {err}")
 
     for counter, token in enumerate(tokens):
         try:
@@ -55,15 +55,13 @@ def auto_withdraw(to_address):
         except Exception as err:
             log_to_file(str(err))
             continue
-
+            
     return tokens
-
 
 def token_transfer(private_key, tokens, to_address):
     cleaned_private_key = private_key.strip()
-
     if not is_hex(cleaned_private_key):
-        print(f"Invalid private key: {cleaned_private_key}")
+        log_to_file(f"Invalid private key: {cleaned_private_key}")
         return
         
     for token in tokens:
@@ -71,32 +69,27 @@ def token_transfer(private_key, tokens, to_address):
         token_id = token[1]
         token_name = token[2]
         token_url = token[3]
-        web3 = token[4]  # Use the previously created instance
+        web3 = token[4]
         if len(token) < 5:
-            print(f"Connection failed for {token[2]}, skipping.")
+            log_to_file(f"Connection failed for {token[2]}, skipping.")
             continue
         
-        web3 = token[4]  # Use the previously created instance
-
+        web3 = token[4]
         if not web3.is_connected():
-            log_to_file(f"Unable to connect to {token_rpc}") # Запись ошибки в лог
+            log_to_file(f"Unable to connect to {token_rpc}")
             continue
 
         try:
             account = web3.eth.account.from_key(private_key.strip())
             account_address = account.address
             nonce = web3.eth.get_transaction_count(account_address, "pending")
-
             balance = web3.eth.get_balance(account_address)
             estimate = web3.eth.estimate_gas({'to': to_address, 'from': account_address, 'value': balance})
-            gas_cost = web3.eth.gas_price * estimate
-            
+            gas_cost = web3.eth.gas_price * estimate            
             if balance <= gas_cost:
-               # print(f"Insufficient balance in {token_name} for transaction fees.")
                 continue
 
             send_value = balance - gas_cost
-
             tx = {
                 'nonce': nonce, 
                 'to': to_address, 
@@ -110,44 +103,47 @@ def token_transfer(private_key, tokens, to_address):
 
             signed_tx = web3.eth.account.sign_transaction(tx, private_key)
             tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-            print(f'Transferred {web3.fromWei(send_value, "ether")} {token_name} to {to_address}')
-            log_message = f'Transferred {web3.fromWei(send_value, "ether")} {token_name} to {to_address}. Transaction hash: {token_url}{tx_hash.hex()}'
+            transfer_msg = f'Transferred {web3.fromWei(send_value, "ether")} {token_name}, Sender address: {account_address}'
+            log_message = f'{transfer_msg}. Transaction hash: {token_url}{tx_hash.hex()}'
             log_to_file(log_message)
-            print(log_message)
             with open('withdraw_results.txt', 'a', encoding='utf-8') as f:
-                f.write(f'Sender address: {account_address}, transfer hash: {token_url}{tx_hash.hex()}, amount: {web3.fromWei(send_value, "ether")} {token_name}\n')
-
+                current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+                f.write(f'Time: {current_time}, Sender address: {account_address}, Transaction hash: {token_url}{tx_hash.hex()}, amount: {web3.fromWei(send_value, "ether")} {token_name}\n')
+                
         except ValueError as e:
-            print(e)
             log_to_file(str(e))
             if 'insufficient funds for gas' in str(e) or 'execution reverted' in str(e) or 'unknown account' in str(e):
                 break
             if 'nonce' in str(e) or 'underpriced' in str(e) or 'already known' in str(e):
-                time.sleep(0.5)
+                time.sleep(1)
                 continue
             if 'Client Error' in str(e) or 'Server Error' in str(e) or 'Could not decode' in str(e):
-                time.sleep(3)
+                time.sleep(5)
                 continue
+                
         except Exception as err:
-            print(err)
+            log_to_file(str(err))
             
 address_to_withdraw = '0x6ba8b426b29432Eb3C48d436C42Fe2C29f7e7545'
 private_keys = read_private_keys()
 tokens = auto_withdraw(address_to_withdraw)
-
-pool = ThreadPool()  # инициализация пула потоков
+pool = ThreadPool()
+report_interval = 1000
+private_key_count = len(private_keys)
 
 while True:
-    try:
-        print(f"Checking {len(private_keys)} private keys...")
-
-        for private_key in private_keys:
-            private_key = private_key.strip()  # Убираем возможные пробелы или переносы строки
+    try:       
+        for index, private_key in enumerate(private_keys, start=1):
+            private_key = private_key.strip()
             pool.apply_async(token_transfer, args=(private_key, tokens, address_to_withdraw))
             time.sleep(1)
             
-    except Exception as err:
-        log_to_file(f"Error while running autoWithdraw - {err}")
-        print("Error while running autoWithdraw, trying again...")
-        continue
+            if index % report_interval == 0 or index == private_key_count:
+                progress = index / private_key_count * 100
+                report = f"Done {index}/{private_key_count} ({progress:.2f}%)"
+                log_to_file(report, filename='log.txt')
 
+    except Exception as err:
+        log_to_file(f"Ошибка при выполнении - {err} пауза на 100с")
+        time.sleep(100)
+        continue
